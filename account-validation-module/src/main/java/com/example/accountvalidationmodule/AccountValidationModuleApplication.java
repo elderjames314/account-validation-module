91a56b1f89131336c85b9c8b30cdd4a7cdf8027a
James OLADIMEJI <joladimeji@ecobank.com>
2021-12-19 06:47:11 +0100

    first commit

---
+++ account-validation-module/src/main/java/com/example/accountvalidationmodule/AccountValidationModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.accountvalidationmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountValidationModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountValidationModuleApplication.class, args);
+    }
+
+}
---
+++ account-validation-module/src/test/java/com/example/accountvalidationmodule/AccountValidationModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accountvalidationmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountValidationModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
